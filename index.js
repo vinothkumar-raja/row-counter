f0df9bc10b6dcd0ba008eaaf2bfd46feda926b38
Vinothkumar <68070917+vinothkumar-raja@users.noreply.github.com>
2023-09-01 18:54:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+let countEl = document.getElementById("count-el")
+let prevEntry = document.getElementById("prev-entry")
+let count = 0
+let savedCount = 0
+let realTimeCount = 0
+	
+function increment() {
+    count = count + 1
+    countEl.innerText = count
+}
+
+function save() {
+	let resetCount = countEl.innerText
+	realTimeCount = +realTimeCount + +resetCount
+	
+	console.log("My Test " + +5 + +6)
+	console.log("My Test" + +5 + +6)
+
+	if(savedCount === 0) {
+		savedCount = realTimeCount
+		prevEntry.innerText = savedCount
+	} else {
+		savedCount = savedCount + '-' + realTimeCount
+		prevEntry.innerText = savedCount
+	}
+	console.log(savedCount)
+	countEl.innerText = 0
+	count = 0
+	
+}
+
+// 1. Create a function, save(), which logs out the count when it's called
+
+
